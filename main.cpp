da7d6074f221223e7a6bdfaad894870c941412d5
DAVMONT08 <davide.montella@marconibari.edu.it>
2025-01-08 22:44:15 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,65 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+              vector<string< v(5);
+              vector<string< w(5);
+              vector<float< x(10);
+              vector<float< y(10);
+              int i,p,d;
+              i=o
+              while(i<5)
+              {
+                v[0]="ghali";
+                v[1]="max pezzali";
+                v[2]="gue";
+                v[3]="jim morrison";
+                v[4]="elvey spresley";
+                cout<<"il" <<i+1 << "cantante è" <<v[i];
+                i=i+1;
+              }
+              i=0;
+              while(i<5)
+              {
+                cout<<"inserire nome cantante" <<i+1 <<"cantante";
+                cin>> w[i];
+                cout<<"il" <<i+1 << "cantante è" << w[i];
+                i=i+1;
+              }
+              i=1;
+              while(i<10)
+              {
+                x[i]=i;
+                if(x[i]%2=0)
+                {
+                    p=x[i];
+                    cout<<"il vettore X con indice pari cella numero" << i << ":" <<p;
+                    cout<<"il vettore X con valore pari cella numero" << i << ":" <<p;
+                }
+                  else
+                  {
+                    d=x[i];
+                    cout<<"il vettore X con indice dispari cella numero" << i << ":" <<d;
+                  }
+                i=i+1;
+              }
+            i=0;
+            while(i*2<=10)
+            {
+                y[i]=i*2;
+                cout<<"il vettore Y cella numero" <<i+1<< ":" <<y[i];
+                i=i+1;
+            }
+
+
+
+
+
+
+
+
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
